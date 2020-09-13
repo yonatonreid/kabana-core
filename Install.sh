05f917246d1559cd1712f51addf2f3c2ebc6e263
yonatonreid <yonatonreid@gmail.com>
2020-09-12 23:49:08 -0400

    Maven installation

--- Install.sh
+++ Install.sh
@@ -13,18 +13,38 @@ apt autoremove
 
 ### Curl Installation ###
 apt-get build-dep curl
-cd /usr/local/src && mkdir curl && cd /usr/local/src/curl
+cd /usr/local/src && mkdir curl && cd /usr/local/src/curl || exit
 wget http://curl.haxx.se/download/curl-7.72.0.tar.bz2
 tar -xvjf curl-7.72.0.tar.bz2
-cd curl-7.72.0
+cd curl-7.72.0 || exit
 ./configure && make && make install
 ldconfig
 
+### Java Installation ###
+add-apt-repository ppa:webupd8team/java
+apt-get update
+apt-get install oracle-java9-installer
+echo "JAVA_HOME=/usr/lib/jvm/java-9-oracle" >> /etc/environment
+source /etc/environment
+
+### Maven Installation ###
+wget https://www-us.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz -P /tmp
+tar xf /tmp/apache-maven-3.6.3-bin.tar.gz -C /opt
+ln -s /opt/apache-maven-3.6.3 /opt/maven
+touch /etc/profile.d/maven.sh
+echo "export M2_HOME=/opt/maven" >> /etc/profile.d/maven.sh
+echo "export MAVEN_HOME=/opt/maven" >> /etc/profile.d/maven.sh
+echo "export PATH=${M2_HOME}/bin:${PATH}" >> /etc/profile.d/maven.sh
+chmod +x /etc/profile.d/maven.sh
+source /etc/profile.d/maven.sh
+
 ### Golang Installation ###
-cd /usr/local/src && mkdir golang && cd /usr/local/src/golang
+cd /usr/local/src && mkdir golang && cd /usr/local/src/golang || exit
 wget https://golang.org/dl/go1.15.2.linux-amd64.tar.gz
 tar xvf go1.15.2.linux-amd64.tar.gz
 mv /usr/local/src/golang/go /usr/local/go
+echo "export PATH=$PATH:$(go env GOPATH)/bin" >> /root/.profile
+source /root/.profile
 
 ### Docker Installation ###
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add –
