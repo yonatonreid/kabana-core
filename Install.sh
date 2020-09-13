56a7d510ae91c00a3b6c86a9075430aa3c074a21
yonatonreid <yonatonreid@gmail.com>
2020-09-13 00:03:40 -0400

    Various installer updates

--- Install.sh
+++ Install.sh
@@ -14,6 +14,7 @@ apt-get install -y libssl-dev libffi-dev python-dev python3-pip tree
 apt-get install -y libxml2 libxml2-dev libxml2-utils libaprutil1 libaprutil1-dev
 apt-get install -y ipv6toolkit net-tools libssl-dev snapd
 apt autoremove
+echo "Ubuntu Software Installed Successfully"
 
 ### Curl Installation ###
 apt-get build-dep curl
@@ -22,6 +23,7 @@ tar -xvjf /tmp/curl-7.72.0.tar.bz2 -C /usr/local/src
 cd curl-7.72.0 || exit
 ./configure && make && make install
 ldconfig
+echo "Curl Installed Successfully"
 
 ### Java Installation ###
 add-apt-repository ppa:webupd8team/java
@@ -29,6 +31,7 @@ apt-get update
 apt-get install oracle-java9-installer
 echo "JAVA_HOME=/usr/lib/jvm/java-9-oracle" >> /etc/environment
 source /etc/environment
+echo "Oracle Java JDK 9 Installed Successfully"
 
 ### Maven Installation ###
 wget https://www-us.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz -P /tmp
@@ -40,6 +43,7 @@ echo "export MAVEN_HOME=/opt/maven" >> /etc/profile.d/maven.sh
 echo "export PATH=${M2_HOME}/bin:${PATH}" >> /etc/profile.d/maven.sh
 chmod +x /etc/profile.d/maven.sh
 source /etc/profile.d/maven.sh
+echo "Maven Installed Successfully"
 
 ### Redis Installation ###
 apt install redis-server
@@ -65,17 +69,20 @@ tar xvf go1.15.2.linux-amd64.tar.gz -C /usr/local/src
 mv /usr/local/src/go /usr/local/go
 echo "export PATH=$PATH:$(go env GOPATH)/bin" >> /root/.profile
 source /root/.profile
+echo "Golang Installed Successfully"
 
 ### Docker Installation ###
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add –
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
 apt-get update
 apt-get install docker-ce
+echo "Docker Installed Successfully"
 
 ### SSH-Agent Installation ###
 sshAgentBinaryLocation=$(which ssh-agent)
 eval $($sshAgentBinaryLocation -s)
 sshAddBinaryLocation=$(which ssh-add)
-$sshAddBinaryLocation /root/.ssh/myssh
+$sshAddBinaryLocation /root/.ssh/id_rsa
+echo "SSH Key Added to SSH Agent"
 
 exit 0
