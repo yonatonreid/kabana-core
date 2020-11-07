a42bed4bed68faa1f46a0346a3338d69763e557b
yonatonreid <yonatonreid@gmail.com>
2020-11-07 05:40:08 -0500

    added kabana root definition

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
-declare(strict_types = 1);
-declare(ticks = 100);
+declare(strict_types=1);
+declare(ticks=100);
 error_reporting(E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR);
 set_time_limit(0);
 umask(0);
@@ -9,4 +9,5 @@ date_default_timezone_set('UTC');
 session_cache_limiter('nocache');
 
 $rootPath = realpath(dirname(__FILE__));
-require_once $rootPath.'/vendor/autoload.php';
+define('KABANA_ROOT', $rootPath);
+require_once $rootPath . '/vendor/autoload.php';
